9ec026aebc36f6bfe5bd19b5b16829db06e0f3ca
fjcarmo <franciscojccarmo@gmail.com>
2021-02-08 22:09:58 +0100

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -28,90 +28,97 @@ namespace API.Controllers
             _userManager = userManager;
         }
 
-    [HttpGet]
-    [Authorize]
-    public async Task<ActionResult<UserDto>> GetCurrentUser()
-    {
-
-        var user = await _userManager.FindByEmailFromClaimsPrinciple(HttpContext.User);
-
-        return new UserDto
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            Email = user.Email,
-            Token = _tokenService.CreateToken(user),
-            DisplayName = user.DisplayName
-        };
-    }
 
-    [HttpGet("emailexists")]
-    public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
-    {
-        return await _userManager.FindByEmailAsync(email) != null;
-    }
+            var user = await _userManager.FindByEmailFromClaimsPrinciple(HttpContext.User);
 
-    [HttpGet("address")]
-    [Authorize]
-    public async Task<ActionResult<AddressDto>> GetUserAddress()
-    {
-        var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                DisplayName = user.DisplayName
+            };
+        }
 
-        return _mapper.Map<Address, AddressDto>(user.Address);
-    }
-
-    [Authorize]
-    [HttpPut("address")]
-    public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
-    {
-        var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
-
-        user.Address = _mapper.Map<AddressDto, Address>(address);
-
-        var result = await _userManager.UpdateAsync(user);
-
-        if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
-
-        return BadRequest("Problem updating the user");
-    }
-
-    [HttpPost("login")]
-    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-    {
-        var user = await _userManager.FindByEmailAsync(loginDto.Email);
-
-        if (user == null) return Unauthorized(new ApiResponse(401));
-
-        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-
-        if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
-
-        return new UserDto
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
         {
-            Email = user.Email,
-            Token = _tokenService.CreateToken(user),
-            DisplayName = user.DisplayName
-        };
-    }
+            return await _userManager.FindByEmailAsync(email) != null;
+        }
 
-    [HttpPost("register")]
-    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-    {
-        var user = new AppUser
+        [HttpGet("address")]
+        [Authorize]
+        public async Task<ActionResult<AddressDto>> GetUserAddress()
         {
-            DisplayName = registerDto.DisplayName,
-            Email = registerDto.Email,
-            UserName = registerDto.Email
-        };
+            var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
 
-        var result = await _userManager.CreateAsync(user, registerDto.Password);
+            return _mapper.Map<Address, AddressDto>(user.Address);
+        }
 
-        if (!result.Succeeded) return BadRequest(new ApiResponse(400));
-
-        return new UserDto
+        [Authorize]
+        [HttpPut("address")]
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
         {
-            DisplayName = user.DisplayName,
-            Token = _tokenService.CreateToken(user),
-            Email = user.Email
-        };
+            var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
+
+            user.Address = _mapper.Map<AddressDto, Address>(address);
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+
+            return BadRequest("Problem updating the user");
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                DisplayName = user.DisplayName
+            };
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new [] {"Email address is in use"}
+                });
+            }
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Token = _tokenService.CreateToken(user),
+                Email = user.Email
+            };
+        }
     }
-}
 }
